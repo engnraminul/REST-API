876fbab5e77f73383bf04c4506a958a5aa7c4448
engnraminul <engnraminul@gmail.com>
2022-05-11 22:09:16 +0600

    Create User login API simple system

--- API_app/urls.py
+++ API_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import firstAPI
+from .views import firstAPI, registrationAPI
 
 urlpatterns = [
     path('profile/', firstAPI),
+    path('registration/', registrationAPI),
 ]
--- API_app/views.py
+++ API_app/views.py
@@ -1,3 +1,4 @@
+import email
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
@@ -15,3 +16,37 @@ def firstAPI(request):
     }
 
     return Response(context)
+
+
+
+#Create User Login API
+from django.contrib.auth.models import User
+@api_view(['POST',])
+def registrationAPI(request):
+    if request.method=="POST":
+        username=request.data['username']
+        email=request.data['email']
+        first_name = request.data['first_name']
+        last_name = request.data['last_name']
+        password1=request.data['password1']
+        password2=request.data['password2']
+
+        if User.objects.filter(username=username).exists():
+            return Response({"error":"username already exists!"})
+        
+        if password1 != password2:
+            return Response({"error":"Password didn't matched!"})
+
+        
+        user=User()
+
+        user.username=username
+        user.email=email
+        user.first_name=first_name
+        user.last_name=last_name
+        user.is_active=True
+
+        user.set_password(raw_password=password1)
+        user.save()
+
+        return Response({"Success":"User Successfully Registered"})
