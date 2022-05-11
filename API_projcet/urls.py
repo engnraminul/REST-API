f0be01afe87ab12b129c4ea7f7588fd067605e97
engnraminul <engnraminul@gmail.com>
2022-05-11 23:49:19 +0600

    Create simpleJWT

--- API_projcet/urls.py
+++ API_projcet/urls.py
@@ -4,11 +4,17 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('rest_framework.urls')),
+    path('login-token/', obtain_auth_token),
     path('app/', include('API_app.urls')),
+    path('obtain-token/', TokenObtainPairView.as_view()),
+    path('refresh-token/', TokenRefreshView.as_view()),
 ]
 urlpatterns += static(settings.MEDIA_URL, Document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, Document_root=settings.STATIC_ROOT)
